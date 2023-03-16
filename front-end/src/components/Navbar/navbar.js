5f03b8cb56307e4e954535d289a966415f5c075d
Fulxe <muruuuu8@gmail.com>
2023-03-16 16:55:55 +0800

    front end push

---
+++ front-end/src/components/Navbar/navbar.js
@@ -0,0 +1,38 @@
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import "./navbar.css";
+
+function Navbar() {
+  const [categorie, setCategorie] = useState(false);
+  return (
+    <div className="navbar-main">
+      <div className="Navbar">
+        <div className="nav-logo">
+          <h3>Logo</h3>
+          <div></div>
+        </div>
+        <div className="nav-log">
+          <button>Sign-up</button>
+          <button>Sign-in</button>
+          <button>
+            About us
+          </button>
+        </div>
+      </div>
+      <div className="nav-angilal">
+        <div className="nav-angi" onClick={() => setCategorie(!categorie)}>
+          <p>АНГИЛАЛ</p>
+        </div>
+      </div>
+      <Outlet />
+      {categorie && (
+        <div className="Categories">
+          <button onClick={() => setCategorie(!categorie)}>Cancel</button>
+          <div className="cat-heading"></div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
