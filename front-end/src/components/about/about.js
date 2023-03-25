fbd8b1ec2f16ce08038097744591b2dc9302e513
Tuuguush <tuuguush@Tuuguushs-MacBook-Pro.local>
2023-03-25 10:14:26 +0800

    aboutus

--- front-end/src/components/about/about.js
+++ front-end/src/components/about/about.js
@@ -1,8 +1,35 @@
 import "./about.css"
+import React from "react";
 
-
-function About () {
-    <div className="About"></div>
+function About() {
+    return(
+    <div className="About">
+        <div className="div1">
+        <div className="Murun">
+            <div className="picture1"></div>
+            <div className="w">Muruu<br/>muruu123@gmail.com</div>
+        </div>
+        <div className="zuras"></div>
+        <div className="Tuuguu">
+            <div className="picture2"></div>
+            <div className="w">Tuuguu<br/>Tuuguu123@gmail.com</div>
+        </div>
+        <div className="zuras"></div>
+        <div className="Bilguun">
+            <div className="picture3"></div>
+            <div className="w">Bilguun<br/>Bilguun123@gmail.com</div>
+        </div>
+        <div className="zuras"></div>
+        <div className="Tengis">
+            <div className="picture4"></div>
+            <div className="w">Tengis<br/>Tengis123@gmail.com</div>
+        </div>
+        <div className="zuras"></div>
+        </div>
+        <div className="logo">
+        </div>
+    </div>
+    );
 }
 
 
