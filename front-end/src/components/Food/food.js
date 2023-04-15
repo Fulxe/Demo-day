540f007d1b63393e4884ea994f112925642e4047
Bilguun Batkhuyag <bilguunbatkhuyag@Bilguuns-MacBook-Pro.local>
2023-04-15 11:25:43 +0800

    category done

--- front-end/src/components/Food/food.js
+++ front-end/src/components/Food/food.js
@@ -7,45 +7,47 @@ function Food() {
   const paths = ["Mongolian", "Vegan", "Healthy", "Korean", "Mexican"];
   const [dropdown, showDropdown] = useState(false);
   return (
-    <div className="Food" onClick={() => showDropdown(false)}>
-      <div className="food">
-        <div className="food-line1">
-          <div class="navbar">
-            <div class="dropdown">
-              <button
-                class="dropbtn"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  showDropdown(!dropdown);
-                }}
-              >
-                Categories
-              </button>
-              {dropdown && (
-                <div
-                  class="dropdown-content"
+    <AnimatedPage>
+      <div className="Food" onClick={() => showDropdown(false)}>
+        <div className="food">
+          <div className="food-line1">
+            <div class="navbar">
+              <div class="dropdown">
+                <button
+                  class="dropbtn"
                   onClick={(e) => {
                     e.stopPropagation();
+                    showDropdown(!dropdown);
                   }}
                 >
-                  {paths.map((path) => (
-                    <div>
-                      <Link className="food-path" to={path.toLowerCase()}>
-                        {path}
-                      </Link>
-                    </div>
-                  ))}
-                </div>
-              )}
+                  Categories
+                </button>
+                {dropdown && (
+                  <div
+                    class="dropdown-content"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                  >
+                    {paths.map((path) => (
+                      <div>
+                        <Link className="food-path" to={path.toLowerCase()}>
+                          {path}
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
+          <div className="food-line2"></div>
+          <div className="food-line3"></div>
+          <div className="food-line1"></div>
         </div>
-        <div className="food-line2"></div>
-        <div className="food-line3"></div>
-        <div className="food-line1"></div>
+        <Footer className="food-footer" />
       </div>
-      <Footer className="food-footer" />
-    </div>
+    </AnimatedPage>
   );
 }
 
